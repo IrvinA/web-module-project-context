9d33e85739fa404060e3fc79f08f2e3d652e6ba1
IrvinA <arevalosirvin@gmail.com>
2021-10-07 14:15:19 -0700

    Mvp complete

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 const Item = props => {
+	const { item } = props;
 	return (
 		<div className="shopping-cart_item">
-			<img src={props.image} alt={`${props.title} book`} />
+			<img src={item.image} alt={`${item.title} book`} />
 
 
 			<div>
-				<h1>{props.title}</h1>
-				<p>$ {props.price}</p>
+				<h1>{item.title}</h1>
+				<p>$ {item.price}</p>
 				<button>Remove from cart</button>
 			</div>
 		</div>
